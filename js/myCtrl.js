9fc750ddc723b595a4941c27c26ed6280d7bee04
gityanxu <1147353251@qq.com>
2018-04-03 10:15:55 +0800

    20180403-10:15

--- js/myCtrl.js
+++ js/myCtrl.js
@@ -11,9 +11,34 @@ yx_mallApp
             //用户头像
             userHeadImg:"images/head.png",
             //用户积分
-            userIntegral:"1000",
-            userInfo:[]
+            user_happiness:0,//严选积分
+            user_money:0,//购物积分
+            userInfo:[],
+            integral_status:false
         };
+        // 查看积分
+        $scope.lookIntegral=function () {
+               $scope.my.integral_status = true;
+        };
+
+        //收起弹出层
+        $scope.lookNo=function () {
+            $scope.my.integral_status = false;
+        };
+        // 请求数据
+        var userAll=appService._postData(URL+"index.php?s=Api/wealth/youpin_my_integral",{ token: localStorage.getItem("tokens"),
+            way:localStorage.getItem("way")});
+        userAll.then(function (e) {
+            if(e.data.data.portrait){
+                $scope.my.userHeadImg=URL+e.data.data.portrait;
+            }
+            $scope.my.user_happiness=e.data.data.happiness;
+            $scope.my.user_money=e.data.data.money;
+           
+        },function (e) {
+
+        });
+
         $scope.my.userInfo = JSON.parse(localStorage.getItem("userInfo"));
         console.log([
             "                   _ooOoo_",
